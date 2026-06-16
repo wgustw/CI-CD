dd7e24768e8febcf70c6cb5d3053df166ae3244e
wgustw <wgustw@github.com>
2026-06-16 15:17:50 +0000

    fix: corrige erro de sintaxe na rota status

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
     return {"mensagem": "API funcionando corretamente"}
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 @app.route("/status")
 def status():
     return {"status": "API online"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
