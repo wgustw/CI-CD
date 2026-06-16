dadc0ebab9cc849786e7de5efca7765759c79ef6
wgustw <wgustw@github.com>
2026-06-16 15:18:23 +0000

    feat: estrutura base do projeto para configurar CI

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"mensagem": "API funcionando corretamente"}
+
+@app.route("/status")
+def status():
+    return {"status": "API online"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
